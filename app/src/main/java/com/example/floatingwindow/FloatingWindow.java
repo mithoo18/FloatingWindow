75d78d626c5dd9a5eb3a84ecbf7f5ddcc372e298
mits <devanshu18g@gmail.com>
2021-05-26 18:48:36 +0530

    third commit

--- app/src/main/java/com/example/floatingwindow/FloatingWindow.java
+++ app/src/main/java/com/example/floatingwindow/FloatingWindow.java
@@ -1,4 +1,103 @@
 package com.example.floatingwindow;
 
-public class FloatingWindow {
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import androidx.annotation.Nullable;
+
+public class FloatingWindow extends Service implements View.OnClickListener {
+    private WindowManager mWindowManager;
+    private View mFloatingView;
+    private View collapsedView;
+    private View expandedView;
+
+    public FloatingWindow() {
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget,null);
+
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mWindowManager.addView(mFloatingView,params);
+
+        collapsedView = mFloatingView.findViewById(R.id.layoutCollapsed);
+        expandedView = mFloatingView.findViewById(R.id.layoutExpanded);
+
+        mFloatingView.findViewById(R.id.buttonClose).setOnClickListener(this);
+        expandedView.setOnClickListener(this);
+
+        mFloatingView.findViewById(R.id.relativeLayoutParent).setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = params.x;
+                        initialY = params.y;
+                        initialTouchX = motionEvent.getRawX();
+                        initialTouchY = motionEvent.getRawX();
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+
+                        collapsedView.setVisibility(View.GONE);
+                        expandedView.setVisibility(View.VISIBLE);
+                        return true;
+
+                     case MotionEvent.ACTION_MOVE:
+                        params.x = initialX + (int) (motionEvent.getRawX() - initialTouchX );
+                        params.x = initialX + (int) (motionEvent.getRawX() - initialTouchX );
+                        mWindowManager.updateViewLayout(mFloatingView,params);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.layoutExpanded:
+                collapsedView.setVisibility(View.VISIBLE);
+                expandedView.setVisibility(View.GONE);
+                break;
+            case R.id.buttonClose:
+                stopSelf();
+                break;
+        }
+    }
 }
